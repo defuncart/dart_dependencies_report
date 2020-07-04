f5b3d30fae85cadfbd0dd5988ce9739895ff8e95
James Leahy <defuncart@gmail.com>
2020-07-04 16:17:38 +0200

    Update PDFGenerator.

--- lib/src/services/pdf_generator.dart
+++ lib/src/services/pdf_generator.dart
@@ -5,6 +5,7 @@ import 'package:meta/meta.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
+import '../extensions/dependency_type_extensions.dart';
 import '../models/dependency.dart';
 import '../models/report_content.dart';
 
@@ -14,31 +15,110 @@ class PDFGenerator {
 
   /// Creates and exports a report with [ReportContent] content at [outputFilepath]
   static Future<void> createExport({@required ReportContent content, String outputFilepath}) async {
-    pw.Widget _tableForDeps(context, {@required List<Dependency> deps}) => deps.isEmpty
-        ? pw.Text('None')
-        : pw.Table.fromTextArray(
-            context: context,
-            headerCount: 1,
-            data: <List<String>>[
-              <String>['Name', 'Version', 'License', 'Score'],
-              for (final dep in deps)
-                <String>[
-                  dep.name,
-                  if (dep.version != null) ...[
-                    dep.version ?? '',
-                    dep.license ?? '',
-                    dep.score ?? '',
-                  ],
-                  if (dep.version == null) 'Part of sdk',
-                ],
-            ],
-            columnWidths: {
-              0: pw.IntrinsicColumnWidth(flex: 3),
-              1: pw.IntrinsicColumnWidth(flex: 1),
-              2: pw.IntrinsicColumnWidth(flex: 1),
-              3: pw.IntrinsicColumnWidth(flex: 1),
-            },
-          );
+    pw.Widget _generateTableNameVersionLicenseScore(
+      pw.Context context, {
+      @required String title,
+      @required List<Dependency> deps,
+    }) =>
+        pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Header(level: 1, text: title),
+            deps.isEmpty
+                ? pw.Text('None')
+                : pw.Table.fromTextArray(
+                    context: context,
+                    headerCount: 1,
+                    data: <List<String>>[
+                      <String>['Name', 'Version', 'License', 'Score'],
+                      for (final dep in deps)
+                        <String>[
+                          dep.name,
+                          if (dep.version != null) ...[
+                            dep.version ?? '',
+                            dep.license ?? '',
+                            dep.score ?? '',
+                          ],
+                          if (dep.version == null) 'Part of sdk',
+                        ],
+                    ],
+                    columnWidths: {
+                      0: pw.IntrinsicColumnWidth(flex: 3),
+                      1: pw.IntrinsicColumnWidth(flex: 1),
+                      2: pw.IntrinsicColumnWidth(flex: 1),
+                      3: pw.IntrinsicColumnWidth(flex: 1),
+                    },
+                  ),
+          ],
+        );
+
+    pw.Widget _generateTableNameVersionType(
+      pw.Context context, {
+      @required String title,
+      @required List<Dependency> deps,
+    }) =>
+        pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Header(level: 1, text: title),
+            deps.isEmpty
+                ? pw.Text('None')
+                : pw.Table.fromTextArray(
+                    context: context,
+                    headerCount: 1,
+                    data: <List<String>>[
+                      <String>['Name', 'Version', 'Type'],
+                      for (final dep in deps)
+                        <String>[
+                          dep.name,
+                          dep.version,
+                          dep.type.displayName,
+                        ],
+                    ],
+                    columnWidths: {
+                      0: pw.IntrinsicColumnWidth(flex: 2),
+                      1: pw.IntrinsicColumnWidth(flex: 1),
+                      2: pw.IntrinsicColumnWidth(flex: 1),
+                    },
+                  ),
+          ],
+        );
+
+    pw.Widget _generateTableNameVersionTypeLicenseRef(
+      pw.Context context, {
+      @required String title,
+      @required List<Dependency> deps,
+    }) =>
+        pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Header(level: 1, text: title),
+            deps.isEmpty
+                ? pw.Text('None')
+                : pw.Table.fromTextArray(
+                    context: context,
+                    headerCount: 1,
+                    data: <List<String>>[
+                      <String>['Name', 'Version', 'Type', 'License', 'Ref'],
+                      for (final dep in deps)
+                        <String>[
+                          dep.name,
+                          dep.version,
+                          dep.type.displayName,
+                          dep.license,
+                          dep.ref,
+                        ],
+                    ],
+                    columnWidths: {
+                      0: pw.IntrinsicColumnWidth(flex: 2),
+                      1: pw.IntrinsicColumnWidth(flex: 1),
+                      2: pw.IntrinsicColumnWidth(flex: 1),
+                      3: pw.IntrinsicColumnWidth(flex: 1),
+                      4: pw.IntrinsicColumnWidth(flex: 2),
+                    },
+                  ),
+          ],
+        );
 
     final doc = pw.Document();
     doc.addPage(
@@ -107,58 +187,30 @@ class PDFGenerator {
                 ],
             ],
           ),
-          // Direct Dependencies
+          // user defined
+          for (final kvp in content.userDefinedGroupsDeps.entries)
+            pw.Column(
+              children: [
+                pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
+                _generateTableNameVersionType(context, title: kvp.key, deps: kvp.value),
+              ],
+            ),
+          // direct
           pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
-          pw.Header(level: 1, text: 'Direct Dependencies'),
-          _tableForDeps(context, deps: content.directDeps),
-          // Dev Dependencies
+          _generateTableNameVersionLicenseScore(context, title: 'Direct Dependencies', deps: content.directDeps),
+          // dev
           pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
-          pw.Header(level: 1, text: 'Dev Dependencies'),
-          _tableForDeps(context, deps: content.devDeps),
-          // Git Hosted
+          _generateTableNameVersionLicenseScore(context, title: 'Dev Dependencies', deps: content.devDeps),
+          // git
           pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
-          pw.Header(level: 1, text: 'Git Hosted'),
-          pw.Table.fromTextArray(
-            context: context,
-            headerCount: 1,
-            data: <List<String>>[
-              <String>['Name', 'Version', 'License', 'Ref'],
-              for (final dep in content.gitDeps)
-                <String>[
-                  dep.name,
-                  dep.version,
-                  dep.license,
-                  dep.ref,
-                ],
-            ],
-            columnWidths: {
-              0: pw.IntrinsicColumnWidth(flex: 2),
-              1: pw.IntrinsicColumnWidth(flex: 1),
-              2: pw.IntrinsicColumnWidth(flex: 1),
-              3: pw.IntrinsicColumnWidth(flex: 2),
-            },
-          ),
-          // Transitive Dependencies
+          _generateTableNameVersionTypeLicenseRef(context, title: 'Git Hosted', deps: content.gitDeps),
+          // transitive
           pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
-          pw.Header(level: 1, text: 'Transitive Dependencies'),
-          _tableForDeps(context, deps: content.transitiveDeps),
-          // Path Dependencies
+          _generateTableNameVersionLicenseScore(context,
+              title: 'Transitive Dependencies', deps: content.transitiveDeps),
+          // path
           pw.Padding(padding: const pw.EdgeInsets.symmetric(vertical: 8)),
-          pw.Header(level: 1, text: 'Path Dependencies'),
-          content.pathDeps.isEmpty
-              ? pw.Text('None')
-              : pw.Table.fromTextArray(
-                  context: context,
-                  headerCount: 1,
-                  data: <List<String>>[
-                    <String>['Name', 'Version'],
-                    for (final dep in content.pathDeps)
-                      <String>[
-                        dep.name,
-                        dep.version,
-                      ],
-                  ],
-                ),
+          _generateTableNameVersionType(context, title: 'Path Dependencies', deps: content.pathDeps),
         ],
       ),
     );
